ab669e3d418e26f05a3c3826c367a3872812bbb3
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2024-05-22 16:47:27 -0400

    Update autopyinstall.bat

--- autopyinstall.bat
+++ autopyinstall.bat
@@ -1,9 +1,8 @@
 @echo off
-REM Define the URL of the Python installer
-
 REM Download Python
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe', 'python_installer.exe')"
 
 REM Install Python silently
 start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 del python_installer.exe
+echo "Downloads complete!"
