1d37279c803d00d72c8fbd2f18001a7b75cdbbd8
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2024-05-22 18:28:28 -0400

    Update autopyinstall.bat

--- autopyinstall.bat
+++ autopyinstall.bat
@@ -5,5 +5,4 @@ powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www
 start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 del python_installer.exe
 echo Complete!
-sleep 2
-
+timeout /t 2
