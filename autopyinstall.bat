a449396a106397bb5e2416ff29cceea9db98dbe9
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2024-08-29 17:37:04 -0400

    Update autopyinstall.bat

--- autopyinstall.bat
+++ autopyinstall.bat
@@ -1,8 +1,7 @@
 @echo off
 echo Installing.....
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe', 'python_installer.exe')"
-Rem add /quiet below for quiet installation
-start /wait python_installer.exe InstallAllUsers=1 PrependPath=1 Include_test=0
+start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
 del python_installer.exe
 echo Complete!
 timeout /t 2
